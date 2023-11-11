5776b5ebc3a99d03570391d40a62e325f387f8ac
Micheal Palliparambil <mikerambil@gmail.com>
2023-11-11 02:40:39 -0600

    Dynamic import for hero image v4

--- app/components/hero/HeroImage.tsx
+++ app/components/hero/HeroImage.tsx
@@ -1,25 +1,16 @@
-import { m } from 'framer-motion';
 import Image from 'next/image';
 import hero from '/public/images/hero.svg';
-
-const variants = {
-  hidden: { opacity: 0, x: 0, y: 20 },
-  enter: { opacity: 1, x: 0, y: 0, transition: { duration: 1 } },
-  exit: { opacity: 0, x: -0, y: 20 },
-};
 
 export default function HeroImage() {
   return (
     <>
-      <m.div initial='hidden' animate='enter' exit='exit' variants={variants}>
-        <Image
-          alt="Micheal Palliparambil's Picture"
-          src={hero}
-          width={250}
-          height={226}
-          priority
-        />
-      </m.div>
+      <Image
+        alt="Micheal Palliparambil's Picture"
+        src={hero}
+        width={250}
+        height={226}
+        priority
+      />
     </>
   );
 }
