5dd166302cdf4df3e77d81cf1a6191b32e742433
Micheal Palliparambil <mikerambil@gmail.com>
2023-11-11 04:09:00 -0600

    Added Accessibility:v2

--- app/components/Socials.tsx
+++ app/components/Socials.tsx
@@ -6,6 +6,7 @@ export default function Socials() {
         rel='noopener'
         target='_blank'
         href='https://github.com/mike-rambil'
+        aria-label='GitHub Profile'
       >
         {/* @ts-ignore */}
         <box-icon
@@ -23,6 +24,7 @@ export default function Socials() {
         target='_blank'
         className='text-black'
         rel='noopener'
+        aria-label='LinkedIn Profile'
       >
         {/* @ts-ignore */}
         <box-icon
@@ -39,6 +41,7 @@ export default function Socials() {
         rel='noopener'
         target='_blank'
         href='https://instagram.com/three_miles_out?igshid=YzAwZjE1ZTI0Zg%3D%3D&utm_source=qr'
+        aria-label='Instagram Profile'
       >
         {/* @ts-ignore */}
         <box-icon
@@ -55,6 +58,7 @@ export default function Socials() {
         rel='noopener'
         target='_blank'
         href='mailto:mikerambil@gmail.com'
+        aria-label='Email'
       >
         {/* @ts-ignore */}
         <box-icon
