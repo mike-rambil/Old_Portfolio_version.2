2512cc3c82dea9380e3f42bfb9f637f11668215c
Micheal Palliparambil <mikerambil@gmail.com>
2023-11-11 02:36:55 -0600

    Dynamic import for hero image v3

--- app/components/hero/HeroImage.tsx
+++ app/components/hero/HeroImage.tsx
@@ -4,7 +4,7 @@ import hero from '/public/images/hero.svg';
 
 const variants = {
   hidden: { opacity: 0, x: 0, y: 20 },
-  enter: { opacity: 1, x: 0, y: 0, transition: { duration: 0.5 } },
+  enter: { opacity: 1, x: 0, y: 0, transition: { duration: 1 } },
   exit: { opacity: 0, x: -0, y: 20 },
 };
 
